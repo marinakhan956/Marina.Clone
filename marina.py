92c5555236c436e6a9c1f03b9f68850148bda8b7
Marinakhan <zendagi.se.maot.behetar.hai@gmail.com>
2025-05-25 14:11:42 +0500

    Update marina.py

--- marina.py
+++ marina.py
@@ -11,7 +11,7 @@ if bit == '64bit':
     from bs64 import main
     main()0.0.2
 elif bit == '32bit':
-    from marina32 import main
+    from bs32 import main
     main()
 else:
     print('\n YOUR DEVICE IS NOT SUPPORT THIS TOOL')
